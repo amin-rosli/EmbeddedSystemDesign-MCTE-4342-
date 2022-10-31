61d34675b79589fe5d12fa4ace86a037e60c87c2
amin-rosli <116183995+amin-rosli@users.noreply.github.com>
2022-10-31 17:15:34 +0800

    Create Example5.ino

---
+++ Assignments/Week 4 GPIO/Example 5/Example5.ino
@@ -0,0 +1,32 @@
+unsigned int count = 0;                       // assign count
+bool prev;                                    // assign prev to track if it is previously pressed
+unsigned char *read = (unsigned char *)0x23;  // assigns *read as port for B
+unsigned char *dir = (unsigned char *)0x24;
+unsigned char *write = (unsigned char *)0x25;
+bool current;                                 // assign current to keep track current output of *read
+unsigned long prev_time = millis();
+
+void setup() {
+  Serial.begin(9600);
+  
+
+  *dir = 0b00000000;   //or *dir = 32 or *dir = 0x20
+  *write = 0b11111111;
+}
+
+void loop() {
+  unsigned long current_time = millis();
+  bool current = (((*read) & 2) != 0);  // assign current to check if *read and dpin 8 is pressed
+  Serial.println(current);
+
+  if (current_time - prev_time > 20) {
+    
+
+    if (current && !prev) {   // checks if current and prev is not the same
+      count = count + 1;      // increases count value
+      Serial.println(count);  // prints count value
+      prev_time = current_time;
+    }
+    prev = current;  // makes prev and current the same value
+  }
+}
